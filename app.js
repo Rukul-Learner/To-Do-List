2af2a8f599034b20974a224be3e6d32a3a620159
Rukul <rukulsingh98@gmail.com>
2022-01-27 23:49:24 +0530

    EJS Template Engine and Dummy Code add

--- app.js
+++ app.js
@@ -3,14 +3,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app=express();
+const app = express();
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get("/",function (req, res) {
-    res.send("Great Work!");
+app.get("/", function(req, res) {
+    var today = new Date();
+    var currentday = today.getDay();
+    var day = "";
+    switch (currentday) {
+        case 0:
+            day="Sunday! XD";
+            break;
+        case 1:
+            day="Monday :(";
+            break;
+        case 2:
+            day="Tuesday :|";
+            break;
+        case 3:
+            day="Wednesday :|";
+            break;
+        case 4:
+            day="Thursday :)";
+            break;
+        case 5:
+            day="Friday :D";
+            break;
+        case 6:
+            day="Saturday ;D";
+            break;
+        default:
+            day="Get back to work idiot!";
+    }
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
-app.listen(3000,func(){
+app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
