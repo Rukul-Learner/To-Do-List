eb8f945f6df71dbc79798d3cfe0488f3a3a47f0c
Rukul-Learner <78303638+Rukul-Learner@users.noreply.github.com>
2022-06-04 01:04:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+var items=['Buy food',"Cook Food", "Eat Food"];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -12,37 +13,22 @@ app.use(bodyParser.urlencoded({
 
 app.get("/", function(req, res) {
     var today = new Date();
-    var currentday = today.getDay();
-    var day = "";
-    switch (currentday) {
-        case 0:
-            day="Sunday! XD";
-            break;
-        case 1:
-            day="Monday :(";
-            break;
-        case 2:
-            day="Tuesday :|";
-            break;
-        case 3:
-            day="Wednesday :|";
-            break;
-        case 4:
-            day="Thursday :)";
-            break;
-        case 5:
-            day="Friday :D";
-            break;
-        case 6:
-            day="Saturday ;D";
-            break;
-        default:
-            day="Get back to work idiot!";
-    }
+    var options = {
+        weekday: "long",
+        day:"numeric",
+        month:"long"
+    };
+    var day=today.toLocaleDateString("en-US", options);
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day, newListItems:items
     });
 });
+
+app.post("/",function(req,res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
